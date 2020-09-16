48470681f9dfffa39453b601d070942c6adb4d6a
dvsoroka <dvsoroka@gmail.com>
2020-09-16 14:33:48 +0300

    Вызвал функцию рисования домика. Финал

--- Lect_2/house_fin.py
+++ Lect_2/house_fin.py
@@ -4,7 +4,7 @@ def main():
 	windowSize(500, 600)
 	paint_house(100, 200, 300, 400)
 	x = 100
-	y = 200
+	y = 300
 	width = 300
 	height = 400
 	run() # запускает холст
@@ -12,18 +12,27 @@ def main():
 def paint_house(x, y, width, height):
 	paint_walls(x, y, width, height//2)
 	paint_roof(x, y, width, height//2)
-	w_height = height//6
+	w_height = height//4
 	w_width = width//2
-	paint_window(x + w_width, y//2 + w_height, w_width, w_height)
+	paint_window(x + w_width//2, y + w_height//2, w_width, w_height)
 
 def paint_walls(x, y, width, height):
 	print("Рисую стны", x, y)
-	pass
+	brushColor("green")
+	rectangle(x, y, x + width, y + height)
 def paint_roof(x, y, width, height):
-	pass
+	brushColor("gold")
+	points = ((x, height), (x + width, height), (x + width//2, y - height//1.6), (x, height))
+	polygon(points)
 def paint_window(x, y, w_width, w_height):
-	pass
+	brushColor("white")
+	rectangle(x, y, x + w_width, y + w_height)
 
 main()
+
+
+# python house_fin.py
+# git add house_fin.py
+# git commit -m #Вызвал функцию рисования домика. Пока это - функция-заглушка
 
 
