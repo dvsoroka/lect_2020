5819795826f9a956e7b7420310f420724bb6e2ad
dvsoroka <dvsoroka@gmail.com>
2020-12-11 12:57:31 +0200

    Генератором списка создаем 5 движущихся шаров.

--- Lect_7/my_balls.py
+++ Lect_7/my_balls.py
@@ -5,8 +5,8 @@ from random import randint
 WIDTH = 300
 HEIGHT = 200
 
-class Ball:
 
+class Ball:
     def __init__(self):
         self.R = randint(20, 50)
         self.x = randint(self.R, WIDTH - self.R)
@@ -16,6 +16,7 @@ class Ball:
                                      self.y - self.R,
                                      self.x + self.R,
                                      self.y + self.R, fill = "green")
+
     def move(self):
         self.x += self.dx
         self.y += self.dy
@@ -26,26 +27,31 @@ class Ball:
 
     def show(self):
         canvas.move(self.ball_id, self.dx, self.dy)
+
         
 def canvas_click_handler(event):
     print('Hello World! x=', event.x, 'y=', event.y)
 
+
 def tick():
-    ball.move()
-    ball.show()
+    for ball in balls:
+        ball.move()
+        ball.show()
     root.after(50, tick)
 
+
 def main():
-    global root, canvas, ball
+    global root, canvas, balls
 
     root = tk.Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
     canvas = tk.Canvas(root)
     canvas.pack(anchor="nw", fill=tk.BOTH)
     canvas.bind('<Button-1>', canvas_click_handler)
-    ball = Ball()
+    balls = [Ball() for i in range(5)]
     tick()
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
